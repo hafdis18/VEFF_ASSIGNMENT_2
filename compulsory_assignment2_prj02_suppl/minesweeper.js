2e6145165200251930203e92e14153c1e087ca5f
hafdis18 <56306203+hafdis18@users.noreply.github.com>
2020-02-25 22:14:20 +0000

    new

--- compulsory_assignment2_prj02_suppl/minesweeper.js
+++ compulsory_assignment2_prj02_suppl/minesweeper.js
@@ -1,20 +1,31 @@
 function Generate(){
     
     var url = 'https://veff213-minesweeper.herokuapp.com/api/v1/minesweeper';
+    var valrows = document.getElementById("valrows").value;
+    var valcols = document.getElementById("valcols").value;
+    var valmines = document.getElementById("valmines").value;
 
-    axios.post(url, {cols: 10, rows:10 , mines:10})
+
+    axios.post(url, {rows: 10, cols: 10 , mines: 10})
                 .then(function (response) {
                     console.log("Success: ", response.data);
 
                     var columns = response.data.board.cols;
                     var rows = response.data.board.rows;
-                    var mines = response.data.board.mines;
+                    var minepos = response.data.board.minePositions[0];
                     var buttonDiv = document.getElementById("MinesweeperBoard");
 
                     for (i = 0; i < rows; i++){
                         for (j =0; j < columns; j++){
                             var button = document.createElement("button");
-                            buttonDiv.appendChild(button);
+                            if (i === minepos[0] && j === minepos[1]){
+                                button.textContent = button["M"];
+                                buttonDiv.appendChild(button);
+                            }
+                            else{
+                                buttonDiv.appendChild(button);
+                            }
+
                         }
                         
                         var br = document.createElement("br")
@@ -24,8 +35,12 @@ function Generate(){
                 })
                 .catch(function (error) {
                     console.log(error);
-                });
+                })
 }
+
+
+
+
 
 
 
