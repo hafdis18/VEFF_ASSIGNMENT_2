26cf59b28c93c87f5e3e289dc8fdccc022c3d378
hafdis18 <56306203+hafdis18@users.noreply.github.com>
2020-02-27 14:28:11 +0000

    valuesKomið

--- compulsory_assignment2_prj02_suppl/minesweeper.js
+++ compulsory_assignment2_prj02_suppl/minesweeper.js
@@ -1,13 +1,8 @@
-var board = document.getElementById("MinesweeperBoard");
-var testMode = false;
-Generate();
-
 function Generate(){
     var url = 'https://veff213-minesweeper.herokuapp.com/api/v1/minesweeper';
     var valrows = document.getElementById("valrows").value;
     var valcols = document.getElementById("valcols").value;
     var valmines = document.getElementById("valmines").value;
-
 
     axios.post(url, {rows: valrows, cols: valcols , mines: valmines})
                 .then(function (response) {
@@ -15,35 +10,51 @@ function Generate(){
                     var columns = response.data.board.cols;
                     var rows = response.data.board.rows;
                     var minePos = response.data.board.minePositions;
-                    board.innerHTML="";
-                    for (var i = 0; i < rows; i++){
-                        row = grid.insertRow(i);
-                        for (var j = 0; j < cols; j++){
-                            cell = row.insertCell(j);
-                            cell.onclick = function(){
-                                clickCell(this);
-                            };
-                            var mine = document.createAttribute("datamine");
-                            mine.value = "false";
-                            cell.setAttributeNode(mine);
-                        }
-                    }
-                    addMines();
+                    makeBoard(rows,columns,minePos);
                 })
                 .catch(function (error) {
                     console.log(error);
-                    // var boardsize = valcols*valrows;
-                    // if (valmines > boardsize){
-                    //     console.log(error);
-                    // }
                 })
+
 }
 
 
 
-function addMines(){
+function makeBoard(rows, columns, minePos){
+    var buttonDiv = document.getElementById("MinesweeperBoard");
+    while (buttonDiv.firstChild){
+        buttonDiv.removeChild(buttonDiv.firstChild);
+    }
+    var buttonDiv = document.getElementById("MinesweeperBoard");
+
+
+    for (i = 0; i < rows; i++){
+        for (j =0; j < columns; j++){
+            var myarr = [[i],[j]];
+            var button = document.createElement("button");
+            button.setAttribute("value", myarr);
+            buttonDiv.appendChild(button);
+        }
+        var br = document.createElement("br")
+        buttonDiv.appendChild(br)
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// function addMines(){
     
-}
+// }
 
 // function makeBoard(rows, columns){
 //     var board = document.getElementById("MinesweeperBoard");
@@ -67,30 +78,7 @@ function addMines(){
 
 // }
 
-// function makeBoard(rows, columns, minePos){
-//     var buttonDiv = document.getElementById("MinesweeperBoard");
-//     while (buttonDiv.firstChild){
-//         buttonDiv.removeChild(buttonDiv.firstChild);
-//     }
-//     var buttonDiv = document.getElementById("MinesweeperBoard");
 
-//     for (i = 0; i < rows; i++){
-//         for (j =0; j < columns; j++){
-//             var button = document.createElement("button");
-//             buttonDiv.appendChild(button);
-//             var mine = document.createAttribute("datamine");
-//             mine.value = "false";
-//         }
-//         var br = document.createElement("br")
-//         buttonDiv.appendChild(br)
-//     }
-//     setMines(rows, columns, minePos);
-// }
-
-
-// function setMines(row, columns, minePos){
-
-// }
 
 
 
